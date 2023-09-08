c9d44062780f4f8da6fdc5909cc468282549f8b7
Sharon Njogu <njogusharon22@gmail.com>
2023-09-08 09:27:12 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n'); // Print a newline character to end the line
+
+    return (0);
+}
+
