f9cbb58c9aa297b4bf8f1e1719462331a5dcdcae
Sharon Njogu <njogusharon22@gmail.com>
2023-09-29 04:18:20 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for _memset */
+char *_memset(char *s, char b, unsigned int n);
+
+/* Function prototype for simple_print_buffer */
+void simple_print_buffer(char *buffer, unsigned int size);
+
+#endif /* MAIN_H */
+
