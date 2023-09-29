676848126b2c81e8fca5d5effe7a55763d9f6655
Sharon Njogu <njogusharon22@gmail.com>
2023-09-29 04:29:43 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ char *_memset(char *s, char b, unsigned int n);
 /* Function prototype for simple_print_buffer */
 void simple_print_buffer(char *buffer, unsigned int size);
 
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 #endif /* MAIN_H */
 
