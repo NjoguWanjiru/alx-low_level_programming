ac90fed6595b9df155ea01b531947b2fe00420cd
Sharon Njogu <njogusharon22@gmail.com>
2023-10-01 06:14:21 -0700

    3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -12,4 +12,13 @@ void positive_or_negative(int i);
 /* Function prototype for largest_number in 2-largest_number.c */
 int largest_number(int a, int b, int c);
 
+/* Function prototype for print_remaining_days in 3-print_remaining_days.c */
+void print_remaining_days(int month, int day, int year);
+
+/* Function prototype for main in 3-main_a.c and 3-main_b.c */
+int main(void);
+
+/* Function prototype for convert_day in 3-convert_day.c */
+int convert_day(int month, int day);
+
 #endif /* MAIN_H */
