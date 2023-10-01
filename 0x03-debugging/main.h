b789b4fa6b482eb2288336c758b3d2349d95ba63
Sharon Njogu <njogusharon22@gmail.com>
2023-10-01 06:04:38 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,4 +9,7 @@ int main(void);
 /* Function prototype for positive_or_negative in positive_or_negative.c */
 void positive_or_negative(int i);
 
+/* Function prototype for largest_number in 2-largest_number.c */
+int largest_number(int a, int b, int c);
+
 #endif /* MAIN_H */
