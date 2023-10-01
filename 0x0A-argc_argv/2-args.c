6f48659e2169e636da9d52981e51937e17e93a07
Sharon Njogu <njogusharon22@gmail.com>
2023-10-01 05:26:45 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
     int i;
