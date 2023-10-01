ec3c3ad2c8b5e93cf77493c9f1aa3359e6dd6f55
Sharon Njogu <njogusharon22@gmail.com>
2023-10-01 05:59:14 -0700

    1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,10 @@
 
 #include <stdio.h>
 
-/* Function prototype for positive_or_negative */
+/* Function prototype for main in 1-main.c */
+int main(void);
+
+/* Function prototype for positive_or_negative in positive_or_negative.c */
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
