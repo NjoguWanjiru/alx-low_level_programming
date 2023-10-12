9e85614a307f1b0e56e8e7948b420c4be8790ca4
Njogu Wanjiru <njogusharon22@gmail.com>
2023-10-12 01:48:38 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,4 +9,8 @@ int _putchar(char c);
 /* Function prototype for the print_name function */
 void print_name(char *name, void (*f)(char *));
 
+/* Function prototypes for the printing functions */
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+
 #endif /* FUNCTION_POINTERS_H */
